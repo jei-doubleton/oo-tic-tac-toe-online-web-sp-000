1aa867bce61b5da04ee222518af69fd22715b491
J <jennifer.singleton@gmail.com>
2019-01-06 20:08:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,4 +21,105 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(index, char)
+    @board[index] = char
+  end
+
+  def position_taken?(index)
+    if @board[index] == "X" || @board[index] == "O"
+      true
+    else
+      false
+    end
+  end
+
+  def valid_move?(index)
+    if !position_taken?(index) && index.between?(0, 8)
+      true
+    else
+      false
+    end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+
+  def turn_count
+    turns = 0
+    @board.each do |space|
+      if space == "X" || space == "O"
+        turns += 1
+      end
+    end
+    turns
+  end
+
+  def current_player
+    if turn_count % 2 == 0
+      "X"
+    else
+      "O"
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      position_taken?(combo[0]) &&
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]]
+    end
+  end
+
+  def full?
+    @board.all? do |position|
+      position == "X" || position == "O"
+    end
+  end
+
+  def draw?
+    if !won? && full?
+      return true
+    else
+      false
+    end
+  end
+
+  def over?
+    if won? || draw? || full?
+      true
+    end
+  end
+
+  def winner
+    if won?
+      winning_index = won?
+      return @board[winning_index[0]]
+    end
+  end
+
+  def play
+    until over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{@winner(board)}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
 end
